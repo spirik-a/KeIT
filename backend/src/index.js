a9e43e2faa3fa29d9d2ac6ecf0994b4bf73e9e2d
SPIRIK <spirik.a.ua@gmail.com>
2026-01-02 13:11:08 +0200

    Serve static frontend and ignore system files

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,9 +10,14 @@ app.use(bodyParser.json());
 // подключаем роут
 app.use("/messages", messagesRouter);
 
+//подключаем статику
+app.use(express.static("src/public"));
+// функционал апи
+/*
 app.get("/", (req, res) => {
   res.send("Сервер работает!");
 });
+*/
 app.listen(PORT, () => {
   console.log(
     `Server running at http://localhost:${PORT}`
